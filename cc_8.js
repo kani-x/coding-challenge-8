d9f106c87eeeb4f89d191014cdd3d7a7d1735454
kani <kanishkas@usf.edu>
2025-02-16 11:49:22 -0500

    Task 2: Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 }
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary: $6950.00"
+
+// Task 2: Product Price After Discount
+
+const calculateDiscount = function(price, discountRate) {
+    return `Final Price: $${(price - (price * discountRate)).toFixed(2)}`;
+};
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50"
+
